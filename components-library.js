b8726d83ad7fe90081d8bf7ec274ec036cd1156f
Bustamante, Nathan <NBustamante@finys.com>
2025-05-14 18:05:01 -0400

    template detailed dropdownlist

--- components-library.js
+++ components-library.js
@@ -26,6 +26,60 @@ class FinysDetailedDropDownList extends HTMLInputElement {
         this.setAttribute('data-value-field', this.getAttribute('data-value-field') || 'id');
         this.setAttribute('data-height', this.getAttribute('data-height') || '300');
         this.setAttribute('data-option-label', this.getAttribute('data-option-label') || "{AgentId: null, DisplayValue: 'Select Agent...', SearchText:'Select Agent...',AgentName: null, AgentCode: null, AgencyName: null, AgencyCode: null, Address:null }");
+        this.setAttribute('data-option-label-template', this.getAttribute('data-option-label-template') || "option-template");
+        this.setAttribute('data-template', this.getAttribute('data-template') || "item-template");
+        this.setAttribute('data-value-template', this.getAttribute('data-value-template') || "value-template");
+        if(!document.getElementById(this.getAttribute('data-template'))) {
+            document.querySelector('body').appendChild(this.getItemTemplate())
+        }
+        if(!document.getElementById(this.getAttribute('data-value-template'))) {
+            document.querySelector('body').appendChild(this.getValueTemplate())
+        }
+        if(!document.getElementById(this.getAttribute('data-option-label-template'))) {
+            document.querySelector('body').appendChild(this.getOptionTemplate())
+        }
+    }
+
+    getValueTemplate() {
+        const script = document.createElement('script');
+        script.setAttribute('id', this.getAttribute('data-value-template'));
+        script.setAttribute('type', 'text/x-kendo-template');
+        script.innerHTML = `
+         <div class="f-templated-item-dropdown-container">
+            <div>#: ${this.getAttribute('data-value-field')} # - #: ${this.getAttribute('data-text-field')} #</div>
+            <div>#: ${this.getAttribute('data-detail-left')} # - #: ${this.getAttribute('data-detail-right')} #</div>
+            <div>#: ${this.getAttribute('data-detail-bottom')} #</div>
+        </div>
+        `
+        return script
+    }
+
+    getOptionTemplate() {
+        const script = document.createElement('script');
+        script.setAttribute('id', this.getAttribute('data-option-label-template'));
+        script.setAttribute('type', 'text/x-kendo-template');
+        script.innerHTML = `
+            <div class="f-templated-item-dropdown-container">
+                <div style="font-size: 0.656rem">&nbsp;</div>
+                <div style="font-size: 1em">Select an option...</div>
+                <div>&nbsp;</div>
+                </div>
+        `
+        return script;
+    }
+
+    getItemTemplate() {
+        const script = document.createElement('script');
+        script.setAttribute('id', this.getAttribute('data-template'));
+        script.setAttribute('type', 'text/x-kendo-template');
+        script.innerHTML = `
+            <div class="f-templated-item-dropdown-container">
+                <div>#: ${this.getAttribute('data-value-field')} # - #: ${this.getAttribute('data-text-field')} #</div>
+                <div>#: ${this.getAttribute('data-detail-left')} # - #: ${this.getAttribute('data-detail-right')} #</div>
+                <div>#: ${this.getAttribute('data-detail-bottom')} #</div>
+            </div>
+        `
+        return script;
     }
 }
 
