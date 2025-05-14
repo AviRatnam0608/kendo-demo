cfcde3a1158811e5a6890007e31edbc9ff511fa9
Bustamante, Nathan <NBustamante@finys.com>
2025-05-14 15:02:47 -0400

    componentizing

--- components-library.js
+++ components-library.js
@@ -1,30 +1,51 @@
-function finysInput({value, source, target}) {
-    const element = document.createElement('input');
-    element.setAttribute('data-role', 'dropdownlist');
-    element.setAttribute('data-text-field', "name");
-    element.setAttribute('data-value-field', 'id');
-    element.setAttribute('data-bind', `
-            value: ${value},
-            source: ${source},
-            isSelected: ${value},
-        `)
-    element.setAttribute('data-option-label', "Select an option");
-    document.querySelector(target).appendChild(element);
-    return element;
-}
+class FinysTimePicker extends HTMLInputElement {
+    constructor() {
+      super();
+      this.setAttribute('data-role', 'timepicker');
+      this.setAttribute('data-component-type', 'modern');
+    }
+  }
 
-class FinysDropDownList extends HTMLElement {
+class FinysDropDownList extends HTMLInputElement {
+    static observedAttributes = ["value"]
+
     constructor(props) {
         super();
-        this.setAttribute('data-role', 'dropdownlist');
-        this.setAttribute('data-text-field', "name");
+        console.log(props)
+        this.classList.add('f-dropdown');
+        this.setAttribute('data-role', this.getAttribute('data-role') || 'dropdownlist');
+        this.setAttribute('data-text-field', 'name');
         this.setAttribute('data-value-field', 'id');
-        this.setAttribute('data-bind', `
-                value: ${value},
-                source: ${source},
-                isSelected: ${value},
-            `)
-        this.setAttribute('data-option-label', "Select an option");
-        this.classList.add('f-dropdown my-dropdown')
+        this.setAttribute('data-height', '300');
+        this.setAttribute('data-option-label', 'Select an option');
     }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(
+          `Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+        );
+      }
 }
+
+  class MyCard extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div class="card">
+          <h2>${this.getAttribute('title')}</h2>
+          <p>${this.getAttribute('content')}</p>
+        </div>
+      `;
+    }
+  }
+
+  customElements.define('my-card', MyCard);
+
+
+
+
+
+
+
+
+customElements.define('finys-time-picker', FinysTimePicker, {extends: 'input'})
+customElements.define('finys-dropdownlist', FinysDropDownList, {extends: 'input'})
