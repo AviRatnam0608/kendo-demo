def201c83776b65f1f1066ddd5f65689a9a9f356
Bustamante, Nathan <NBustamante@finys.com>
2025-05-17 14:23:19 -0400

    add color options for button preview

--- ComponentLibraryPreview/button-preview.js
+++ ComponentLibraryPreview/button-preview.js
@@ -13,6 +13,14 @@ class FinysButtonPreview extends HTMLElement {
         this.button.classList.add(`f-button-${state}`);
     }
 
+    setButtonColor(state) {
+        ['f-button-primary', 'f-button-secondary', 'f-button-tertiary', 'f-button-destructive'].forEach((item) => {
+            this.button.classList.remove(item);
+
+        })
+        this.button.classList.add(`f-button-${state}`);
+    }
+
     init() {
         this.button = document.createElement('button', { is: 'finys-button' });
         this.button.setAttribute('id', 'feature-button');
@@ -29,10 +37,32 @@ class FinysButtonPreview extends HTMLElement {
 class FinysButtonControls extends HTMLElement {
     registeredListeners = [];
 
+    static appendChildren(node, elements) {
+        elements.forEach(element => {
+            node.appendChild(element);
+        })
+    }
+
     connectedCallback() {
-        this.createResizeSmallButton();
-        this.createResizeMediumButton();
-        this.createResizeLargeButton();
+        this.buttonContainer1 = document.createElement('section');
+        this.buttonContainer2 = document.createElement('section');
+        this.buttonContainer1.setAttribute('id', 'size-buttons');
+        this.buttonContainer2.setAttribute('id', 'color-buttons');
+        this.constructor.appendChildren(this.buttonContainer1, [
+            this.createResizeSmallButton(),
+            this.createResizeMediumButton(),
+            this.createResizeLargeButton(),
+        ])
+        this.constructor.appendChildren(this.buttonContainer2, [
+            this.createSetPrimaryButton(),
+            this.createSetSecondaryButton(),
+            this.createSetTertiaryButton(),
+            this.createSetDestructiveButton(),
+        ])
+        this.constructor.appendChildren(this, [
+            this.buttonContainer1,
+            this.buttonContainer2
+        ])
     }
 
     disconnectedCallback() {
@@ -45,17 +75,25 @@ class FinysButtonControls extends HTMLElement {
         document.querySelector('finys-code-viewer').render();
     }
 
+    setButtonColor = (color) => () => {
+        const buttonPreview = document.querySelector('finys-button-preview');
+        buttonPreview.setButtonColor(color);
+        document.querySelector('finys-code-viewer').render();
+    }
+
+    registerClickListener(object, func) {
+        const destroyFunc = () => object.removeEventListener('click', func);
+        this.registeredListeners.push(destroyFunc);
+        object.addEventListener('click', func);
+    }
+
     createResizeSmallButton() {
         this.makeSmallButton = document.createElement('button', { is: 'finys-button' });
         this.makeSmallButton.setAttribute('id', 'make-small-button');
         this.makeSmallButton.classList.add('f-button-small')
         this.makeSmallButton.textContent = 'Small';
-        const setSize = this.setButtonSize('small');
-        const destroyFunc = () => this.makeSmallButton.removeEventListener('click', setSize)
-        this.registeredListeners.push(destroyFunc);
-        this.makeSmallButton.addEventListener('click', setSize);
-
-        this.appendChild(this.makeSmallButton);
+        this.registerClickListener(this.makeSmallButton, this.setButtonSize('small'))
+        return this.makeSmallButton;
     }
 
     createResizeMediumButton() {
@@ -63,11 +101,8 @@ class FinysButtonControls extends HTMLElement {
         this.makeMediumButton.setAttribute('id', 'make-medium-button');
         this.makeMediumButton.classList.add('f-button-medium')
         this.makeMediumButton.textContent = 'Medium';
-        const setSize = this.setButtonSize('medium');
-        const destroyFunc = () => this.makeMediumButton.removeEventListener('click', setSize)
-        this.registeredListeners.push(destroyFunc);
-        this.makeMediumButton.addEventListener('click', setSize);
-        this.appendChild(this.makeMediumButton);
+        this.registerClickListener(this.makeMediumButton, this.setButtonSize('medium'))
+        return this.makeMediumButton;
     }
 
     createResizeLargeButton() {
@@ -75,18 +110,54 @@ class FinysButtonControls extends HTMLElement {
         this.makeLargeButton.setAttribute('id', 'make-large-button');
         this.makeLargeButton.classList.add('f-button-large')
         this.makeLargeButton.textContent = 'Large';
-        const setSize = this.setButtonSize('large');
-        const destroyFunc = () => this.makeLargeButton.removeEventListener('click', setSize)
-        this.registeredListeners.push(destroyFunc);
-        this.makeLargeButton.addEventListener('click', setSize);
-        this.appendChild(this.makeLargeButton);
+        this.registerClickListener(this.makeLargeButton, this.setButtonSize('large'))
+        return this.makeLargeButton;
+    }
+
+    createSetPrimaryButton() {
+        this.primaryBtn = document.createElement('button', { is: 'finys-button' });
+        this.primaryBtn.setAttribute('id', 'make-primary-button');
+        this.primaryBtn.classList.add('f-button-medium')
+        this.primaryBtn.classList.add('f-button-primary')
+        this.primaryBtn.textContent = 'Primary';
+        this.registerClickListener(this.primaryBtn, this.setButtonColor('primary'))
+        return this.primaryBtn;
+    }
+
+    createSetSecondaryButton() {
+        this.secondaryBtn = document.createElement('button', { is: 'finys-button' });
+        this.secondaryBtn.setAttribute('id', 'make-secondary-button');
+        this.secondaryBtn.classList.add('f-button-medium')
+        this.secondaryBtn.classList.add('f-button-secondary')
+        this.secondaryBtn.textContent = 'Secondary';
+        this.registerClickListener(this.secondaryBtn, this.setButtonColor('secondary'))
+        return this.secondaryBtn;
+    }
+
+    createSetTertiaryButton() {
+        this.tertiaryBtn = document.createElement('button', { is: 'finys-button' });
+        this.tertiaryBtn.setAttribute('id', 'make-tertiary-button');
+        this.tertiaryBtn.classList.add('f-button-medium')
+        this.tertiaryBtn.classList.add('f-button-tertiary')
+        this.tertiaryBtn.textContent = 'Tertiary';
+        this.registerClickListener(this.tertiaryBtn, this.setButtonColor('tertiary'))
+        return this.tertiaryBtn;
+    }
+
+    createSetDestructiveButton() {
+        this.destructiveBtn = document.createElement('button', { is: 'finys-button' });
+        this.destructiveBtn.setAttribute('id', 'make-destructive-button');
+        this.destructiveBtn.classList.add('f-button-medium')
+        this.destructiveBtn.classList.add('f-button-destructive')
+        this.destructiveBtn.textContent = 'Destructive';
+        this.registerClickListener(this.destructiveBtn, this.setButtonColor('destructive'))
+        return this.destructiveBtn;
     }
 
     destroy() {
         this.registeredListeners.forEach((func) => {
             func();
         })
-
     }
 }
 
